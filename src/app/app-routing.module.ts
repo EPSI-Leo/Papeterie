cd2b4b4337c88c56e316d6f5c37dd32895d95388
moi-sixela <106440915+moi-sixela@users.noreply.github.com>
2024-01-29 19:15:39 +0100

    ajout routes et pages acceuil, produits, contact

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AcceuilComponent } from './route-views/acceuil/acceuil.component';
+import { ContactComponent } from './route-views/contact/contact.component';
+import { ProduitsComponent } from './route-views/produits/produits.component';
+
+const routes: Routes = [
+  { path: '', component: AcceuilComponent },
+  { path:'produits' , component: ProduitsComponent },
+  { path:'contact', component: ContactComponent },
+  { path: '**', redirectTo: ''},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AcceuilComponent } from './route-views/acceuil/acceuil.component';
+import { ContactComponent } from './route-views/contact/contact.component';
+import { ProduitsComponent } from './route-views/produits/produits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AcceuilComponent,
+    ContactComponent,
+    ProduitsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
