6855ab7198e93297ce312d5f7e4b2ab961da3f46
samirfoul <foul_s@hotmail.fr>
2024-01-31 09:18:14 +0100

    Formulaire de modification du produit

--- src/app/route-views/produits/produit-details/produit-details.component.ts
+++ src/app/route-views/produits/produit-details/produit-details.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { Produit } from 'src/app/model/produits/produits';
 import { ProduitService } from 'src/app/services/produits.service';
@@ -17,11 +18,44 @@ export class ProduitDetailsComponent {
     private _produitService: ProduitService,
   ) { }
 
+  public produitForm = new FormGroup({
+    nom: new FormControl('', [Validators.required, Validators.maxLength(100)]),
+    texture: new FormControl('', Validators.required),
+    grammage: new FormControl(null) as FormControl<number | null>,
+    prix: new FormControl(null) as FormControl<number | null>,
+    couleur: new FormControl(''),
+  });
+
   ngOnInit(): void {
     this._activeRoute.params.subscribe(params => {
       const produitId = +params['id']; // Convert id to number
       this.produit = this._produitService.getProduit(produitId);
     });
+    this.produitForm.setValue({
+      nom: this.produit?.nom!,
+      texture: this.produit?.texture!,
+      grammage: this.produit?.grammage!,
+      prix: this.produit?.prix!,
+      couleur: this.produit?.couleur!,
+    })
   }
 
+  public onClick() {
+  }
+
+  public shouldShowError(controlName: string) {
+    return !this.produitForm.get(controlName)!.valid &&
+      this.produitForm.get(controlName)!.touched;
+  }
+
+  public onSubmit() {
+    this.produit!.nom = this.produitForm.value.nom!;
+    this.produit!.texture = this.produitForm.value.texture!;
+    this.produit!.grammage = this.produitForm.value.grammage!;
+    this.produit!.prix = this.produitForm.value.prix!;
+    this.produit!.couleur = this.produitForm.value.couleur!;
+
+    this._produitService.updateProduit(this.produit!);
+  }
 }
+// TODO : meilleur typage
