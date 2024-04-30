726ece2576a7cbd7d5830da4fa9dc76e719920de
Gaspard Martin <gaspard.martin.jouannaud@gmail.com>
2024-04-30 12:03:07 +0200

    reverse proxy working =)

--- API/API_Papeterie/Program.cs
+++ API/API_Papeterie/Program.cs
@@ -3,16 +3,27 @@ using Microsoft.EntityFrameworkCore;
 using MySql.EntityFrameworkCore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+var confBuilder = new ConfigurationBuilder();
+confBuilder.AddJsonFile("appsettings.json");
+confBuilder.AddEnvironmentVariables();
+var configuration = confBuilder.Build();
 
+string connectionString = configuration.GetConnectionString("DefaultConnection")
+    .Replace("${DB_SERVER}", configuration["DB_SERVER"])
+    .Replace("${DB_PORT}", configuration["DB_PORT"])
+    .Replace("${DB_USER}", configuration["DB_USER"])
+    .Replace("${DB_PASSWORD}", configuration["DB_PASSWORD"])
+    .Replace("${DB_NAME}", configuration["DB_NAME"]);
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<PapeterieContext>(options =>
 {
-    options.UseMySQL(builder.Configuration.GetConnectionString("DefaultConnection"));
+    options.UseMySQL(connectionString);
 });
 
 
@@ -20,11 +31,8 @@ builder.Services.AddDbContext<PapeterieContext>(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 
